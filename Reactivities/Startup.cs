84d6d1c728bba129ea36506e0b52dd5a8560a2c2
Juan David Castaneda <juandavid147@hotmail.com>
2020-06-29 07:35:49 -0500

    Added client app

--- Reactivities/Startup.cs
+++ Reactivities/Startup.cs
@@ -32,9 +32,17 @@ namespace Reactivities
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
 
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
+
             services.AddControllers();
 
-            
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +59,8 @@ namespace Reactivities
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
